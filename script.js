cc2d8fc96eddcf1d9bcd23e504e0219d17cb5f7f
namya <namyahs004@gmail.com>
2024-05-26 22:57:48 +0530

    dynamic implementation for images and box

--- script.js
+++ script.js
@@ -2,6 +2,87 @@ var box = document.getElementsByClassName("box");
 var img = document.getElementsByTagName("img");
 var currentImage;
 
+
+var level = localStorage.getItem("level");
+var image = document.getElementsByClassName("completeImage")[0];
+var iteration =0;
+if(level == "easy"){
+    UpdateImage(4);
+    image.innerHTML = `<div class="row">
+    <div class="box"></div>
+    <div class="box"></div>
+</div>
+  <div class="row">
+    <div class="box"></div>
+    <div class="box"></div>
+  </div>`
+}
+    else if(level == "medium") {
+        UpdateImage(9);
+        image.innerHTML = `<div class="row">
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box"></div>
+    </div>
+      <div class="row">
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box"></div>
+      </div>
+
+      <div class="row">
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box"></div>
+      </div>`
+}
+else{
+    UpdateImage(16);
+    image.innerHTML = `
+    <div class="row">
+            <div class="box"></div>
+            <div class="box"></div>
+            <div class="box"></div>
+            <div class="box"></div>
+        </div>
+          <div class="row">
+            <div class="box"></div>
+            <div class="box"></div>
+            <div class="box"></div>
+            <div class="box"></div>
+          </div>
+
+          <div class="row">
+            <div class="box"></div>
+            <div class="box"></div>
+            <div class="box"></div>
+            <div class="box"></div>
+          </div>
+          <div class="row">
+            <div class="box"></div>
+            <div class="box"></div>
+            <div class="box"></div>
+            <div class="box"></div>
+        </div>`
+
+}
+
+
+
+function UpdateImage(iteration){
+    var content = document.getElementsByClassName("container")[0];
+    for (let i = 1; i<=iteration; i++){
+        content.innerHTML+= `
+        <img src="./images/hard/image_part_00${i}.jpg" alt="" style="width: 150px;height: 150px;" draggable="true">`
+    
+    }
+}
+
+
+
+
+
+
 Array.from(img).forEach(e =>{
     console.log(e);
     e.addEventListener("dragstart",(a)=>{
@@ -32,4 +113,3 @@ element.append(currentImage);
         
     })
 });
-
